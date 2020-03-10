15c0bf0b880127d920ce14f56307f2cf6fc3931a
Lydia Gregory <ljg2gb@virginia.edu>
2020-03-10 22:04:48 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,6 +4,18 @@ def mario
 end
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = "Thank You Mario! But Our Princess Is In Another Castle!\n"
   puts status
 end
+
+def link
+  take = "It's Dangerous To Go Alone! Take This.\n"
+  puts take
+end
+
+def all_phrases
+  puts mario
+  puts toadstool
+  puts link
+end
+
